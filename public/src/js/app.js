0f46b22acb56210aec9c249be91de09330218be2
jhkim <jhkim@etra.kr>
2017-08-16 13:07:42 +0900

    format

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -3,27 +3,27 @@
 var defaultLanguage = localStorage.getItem('insight-language') || 'en';
 var defaultCurrency = localStorage.getItem('insight-currency') || 'BTC';
 
-angular.module('insight',[
-  'ngAnimate',
-  'ngResource',
-  'ngRoute',
-  'ngProgress',
-  'ui.bootstrap',
-  'ui.route',
-  'monospaced.qrcode',
-  'gettext',
-  'angularMoment',
-  'insight.system',
-  'insight.socket',
-  'insight.api',
-  'insight.blocks',
-  'insight.transactions',
-  'insight.address',
-  'insight.search',
-  'insight.status',
-  'insight.connection',
-  'insight.currency',
-  'insight.messages'
+angular.module('insight', [
+    'ngAnimate',
+    'ngResource',
+    'ngRoute',
+    'ngProgress',
+    'ui.bootstrap',
+    'ui.route',
+    'monospaced.qrcode',
+    'gettext',
+    'angularMoment',
+    'insight.system',
+    'insight.socket',
+    'insight.api',
+    'insight.blocks',
+    'insight.transactions',
+    'insight.address',
+    'insight.search',
+    'insight.status',
+    'insight.connection',
+    'insight.currency',
+    'insight.messages'
 ]);
 
 angular.module('insight.system', []);
